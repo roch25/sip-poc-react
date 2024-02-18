bfa262581b7c460aea4621a4a0aab8fd26a014a5
roch25 <rochellemartins25@gmail.com>
2024-02-18 15:30:10 +0530

    Add logo and h1 to check for styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import './App.css'
 function App() {
   const env = import.meta.env?.VITE_SOME_KEY
   console.log(env)
-  return <>{'hi ' + env}</>
+  return (
+    <>
+      <h1>{'hi ' + env}</h1>
+      <img src="https://app.dev.usemango.co.uk/static/media/usemango_logo.13d8fa05.png" alt="um logo" />
+    </>
+  )
 }
 
 export default App
